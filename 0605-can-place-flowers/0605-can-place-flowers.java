a216585d55ed3e16bfc46a5678864eb929d23c9c
rutuu25 <rutujakatale98@gmail.com>
2026-08-16 22:35:07 +0530

    Time: 1 ms (98.75%), Space: 47.5 MB (94.65%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,12 +1,14 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
         for(int i=0;i<flowerbed.length;i++){
+            // is it first or previous is empty
             if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) &&
             (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)){
+                //is it last or next is empty
                 n--;
-                flowerbed[i]=1;
+                flowerbed[i]=1; //place flowers
             }
         }
-        return n<=0;
+        return n<=0; // if you want to place 2 flowers but you can place 3 therefore <= 0
     }
 }
