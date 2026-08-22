791ae8fdce23890b3cac5290e84ef148d2ce39bf
rutuu25 <rutujakatale98@gmail.com>
2026-08-22 18:52:11 +0530

    Time: 0 ms (100%), Space: 42.8 MB (92.3%) - LeetHub

---
+++ 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int max=nums[0];
+        int sum=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]<nums[i]){
+                sum+=nums[i];
+            }else{
+                sum=nums[i];
+            }
+            max=Math.max(max,sum);
+        }
+        return max;
+    }
+}
